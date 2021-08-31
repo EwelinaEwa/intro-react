99f296c0aa272c72d8c96c2efbd9439faaec86f7
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-08-31 19:37:31 +0200

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import Todo from "./Todo";
+import Form from "./Form";
 
 function App() {
 
-    const initialTodos = [
-        {id:"1", text:"My first todo", status:false},
-        {id:"2", text:"My second todo", status:false},
-        {id:"3", text:"My third todo", status:false}
-    ];
-
-    const [todos, setTodos] = useState(initialTodos);
-    const [checked, setChecked] = useState(
-        new Array(todos.length).fill(false)
-    );
-
-    const handleChange = () => {
-        setChecked(!checked);
-    }
-
-    const list = (
-        <ul style={{listStyle: "none"}}>
-            {todos.map((todo) =>
-                <li key={todo.id}>
-                    <label>
-                        <input type="checkbox" checked={checked.id} onChange={handleChange} style={{marginRight: 10}}/>
-                        {todo.text}
-                    </label>
-                </li>
-            )}
-        </ul>
-    );
-
     return (
-        <div>
-            {list}
-        </div>
-    );
+        <>
+        < Form/>
+        < Todo/>
+        </>
+    )
 }
 
 
---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+    return (
+        <div>
+            <input type="text" placeholder="Write a new todo" />
+            <br />
+            <button>Add todo</button>
+        </div>
+    );
+}
